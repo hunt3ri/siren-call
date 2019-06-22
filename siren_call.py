3f4ac15db972bb41deba8b5344984436a7eae359
Iain Hunter <iain.hunter@outlook.com>
2019-06-22 17:30:08 +0100

    Initial version

---
+++ siren_call.py
@@ -0,0 +1,22 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+
+def send_message():
+    web_hook = os.getenv("SLACK_WEB_HOOK", 'NOT_SET')
+
+    if web_hook == 'NOT_SET':
+        raise ValueError('Web Hook Env Var not set')
+
+    # Send message to slack
+    response = requests.post(web_hook, json={"text": "Hello, World!"})
+
+    print(response.status_code)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print('Sending message')
+    send_message()

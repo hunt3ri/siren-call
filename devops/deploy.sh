2c42e4a89d4882bf413ed84087b0447e00f70cb0
Iain Hunter <iain.hunter@outlook.com>
2019-07-02 23:26:55 +0100

    Correct deploy for master

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -9,18 +9,19 @@ if [ $IS_PULL_REQUEST == true ]
         return
 fi
 
+
 # Only deploy to Staging if we're on develop
 if [ $BRANCH == "develop" ]
+    then
+        echo Please add necessary deploy scripts here to deploy to staging environment
+fi
+
+# Only deploy to Prod if we're on master
+if [ $BRANCH == "master" ]
     then
         # Create a local venv using app only dependencies this will create a smaller deployable
         export PIPENV_IGNORE_VIRTUALENVS=1
         export PIPENV_VENV_IN_PROJECT=true
         pipenv install
         python ./devops/deploy.py --deploy
-fi
-
-# Only deploy to Prod if we're on master
-if [ $BRANCH == "master" ]
-    then
-        echo Please add necessary deploy scripts here to deploy to prod environment
 fi
